4513d4f5934498e79299165bc0647d917741694e
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2018-11-17 07:32:53 +0800

    Local Notification Tutorial

--- LocalPushNotification/Presenter/VCPresenter.swift
+++ LocalPushNotification/Presenter/VCPresenter.swift
@@ -7,12 +7,26 @@
 //
 
 import Foundation
+import UserNotifications
+
 
 protocol VCPresenterDelegate{
+    func ShowRequest(request:UNNotificationRequest);
 }
 class VCPresenter:NSObject{
     var isNotificationAllowed:Bool=false;
     var delegate:VCPresenterDelegate?
     func CreateNotification(){
+        let content = UNMutableNotificationContent()
+        //adding title, subtitle, body and badge
+        content.title = "Hi this is my local request"
+        content.subtitle = "hello my world"
+        content.body = "I am trying to show you how to do local notifications using MVP Design Pattern"
+        content.badge = 1
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        //getting the notification request
+        let request = UNNotificationRequest(identifier: "SimpleNotification", content: content, trigger: trigger)
+        print("Creating Request Please wait....");
+        delegate?.ShowRequest(request: request);
     }
 }
--- LocalPushNotification/ViewController.swift
+++ LocalPushNotification/ViewController.swift
@@ -15,20 +15,33 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         //setting up notification to enable use of notification in app request permission from user
+        self.vcpresenter=VCPresenter();
         self.vcpresenter?.delegate=self;
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge], completionHandler: {didAllow, error in
             self.vcpresenter?.isNotificationAllowed=didAllow
+            UNUserNotificationCenter.current().delegate = self
         })
     }
     @IBAction func ShowNotificationOnClick(_ sender: Any) {
         //show notifications here
-        if self.vcpresenter!.isNotificationAllowed{
-            
-        }
+        self.vcpresenter?.CreateNotification()
     }
 }
 //MARK: VCDelegate func
 extension ViewController:VCPresenterDelegate{
-    
+    func ShowRequest(request: UNNotificationRequest) {
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
+}
+//MARK: UserNotification func
+extension ViewController:UNUserNotificationCenterDelegate{
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        //displaying the ios local notification when app is in foreground
+        print("Will present notif");
+        completionHandler([.alert, .badge, .sound])
+    }
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        print("Yes I Received it!")
+    }
 }
 
